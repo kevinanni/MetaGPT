336d4278e40c1a816d0f10625f1a918a238932bc
kevinmaggie <70731143@qq.com>
2025-03-12 09:13:02 +0800

    init:branch study

---
+++ study/blackjack.py
@@ -0,0 +1,29 @@
+import asyncio
+from metagpt.roles import (
+    Architect,
+    Engineer,
+    ProductManager,
+    ProjectManager,
+)
+from metagpt.team import Team
+
+async def startup(idea: str):
+    company = Team()
+    company.hire(
+        [
+            ProductManager(),
+            Architect(),
+            ProjectManager(),
+            Engineer(),
+        ]
+    )
+    company.invest(investment=3.0)
+    company.run_project(idea=idea)
+
+    await company.run(n_round=5)
+
+def main():
+    asyncio.run(startup(idea="write a cli blackjack game"))
+
+if __name__ == "__main__":
+    main()
